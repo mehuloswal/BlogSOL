5c130d78d3d20e6efcc92eac82d9d83899d7b6e1
Mehul Oswal <mehuloswal21@gmail.com>
2022-03-16 22:38:09 +0530

    first commit

---
+++ programs/Blog/src/lib.rs
@@ -0,0 +1,56 @@
+use anchor_lang::prelude::*;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+// pub mod blog {
+//     use super::*;
+//     pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+//         Ok(())
+//     }
+// }
+pub fn init_blog(ctx: Context<InitBlog>) -> ProgramResult {
+    // get accounts from ctx
+    let blog_account = &mut ctx.accounts.blog_account;
+    let genesis_post_account = &mut ctx.accounts.genesis_post_account;
+    let authority = &mut ctx.accounts.authority;
+
+    // sets the blog state
+    blog_account.authority = authority.key();
+    blog_account.current_post_key = genesis_post_account.key();
+
+    Ok(())
+}
+pub fn signup_user(ctx: Context<SignupUser>, name: String, avatar: String) -> ProgramResult {
+    Ok(())
+}
+
+#[derive(Accounts)]
+pub struct InitBlog {
+    #[account(init, payer = authority, space = 8+32+32)]
+    pub blog_account: Account<'info, BlogState>,
+    #[account(init, payer = authority, space = 8+32+32+32+32+8)]
+    pub genesis_post_account: Account<'info, PostState>,
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct SignupUser<'info> {
+    #[account(init, payer = authority, space = 8 + 40 + 120  + 32)]
+    pub user_account: Account<'info, UserState>,
+    pub authority: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct BlogState {
+    pub current_post_key: Pubkey,
+    pub authority: Pubkey,
+}
+#[account]
+pub struct UserState {
+    pub name: String,
+    pub avatar: String,
+    pub authority: Pubkey,
+}
